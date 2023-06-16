25406066a2aa8502a375fce038ffe05fdb3c198a
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2023-06-16 08:22:09 +0530

    UI implementation for pet adoptation app

--- App.tsx
+++ App.tsx
@@ -1,12 +1,22 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from './app/views/screens/HomeScreen';
+import DetailsScreen from './app/views/screens/DetailsScreen';
+import DrawerNavigator from './app/views/navigators/DrawerNavigator'
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+   <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown:false}}>
+          <Stack.Screen name="HomeScreen" component={DrawerNavigator}/>
+          <Stack.Screen name="DetailsScreen" component={DetailsScreen}/>
+      </Stack.Navigator>
+   </NavigationContainer>
   );
 }
 
